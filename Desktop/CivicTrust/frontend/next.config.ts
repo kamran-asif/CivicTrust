49246ef6c63278265d2bdb924c8735b96be52768
kamran-asif <mdkamranasif789@gmail.com>
2025-09-08 19:08:10 +0530

    Update next.config.ts

--- Desktop/CivicTrust/frontend/next.config.ts
+++ Desktop/CivicTrust/frontend/next.config.ts
@@ -1,14 +1,1 @@
-import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/api/chatbot/chatResponse',
-        destination: 'http://localhost:5001/api/chatbot/chatResponse', // Your Express backend endpoint
-      },
-    ];
-  },
-};
-
-export default nextConfig;
