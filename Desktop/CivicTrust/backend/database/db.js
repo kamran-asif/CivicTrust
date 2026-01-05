a12bd249cc75e5d1be71deec469a3bba6965c32b
kamran-asif <mdkamranasif789@gmail.com>
2026-01-05 22:38:56 +0530

    Update db.js

--- Desktop/CivicTrust/backend/database/db.js
+++ Desktop/CivicTrust/backend/database/db.js
@@ -1,12 +1,22 @@
-import mongoose from 'mongoose'
+import mongoose from 'mongoose';
 
-const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI,);
-    console.log('MongoDB Connected');
-  } catch (error) {
-    console.error('MongoDB Connection Error:', error);
+const connectDB = () => {
+  const uri = process.env.MONGO_URI;
+
+  if (!uri) {
+    console.error('MongoDB URI not found in environment variables');
     process.exit(1);
   }
+
+  mongoose
+    .connect(uri)
+    .then(() => {
+      console.log('MongoDB Connected');
+    })
+    .catch((err) => {
+      console.error('MongoDB Connection Error:', err.message);
+      process.exit(1);
+    });
 };
-module.exports = connectDB;
+
+export default connectDB;
