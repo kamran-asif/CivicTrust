af89a5a069bfc95c9fa45b6738b8d97960eb3a75
kamran-asif <mdkamranasif789@gmail.com>
2026-01-05 22:41:23 +0530

    Refactor UserSchema for improved readability

--- Desktop/CivicTrust/backend/models/UserModel.js
+++ Desktop/CivicTrust/backend/models/UserModel.js
@@ -1,22 +1,35 @@
+
 import mongoose from 'mongoose';
 
-const UserSchema = new mongoose.Schema({
-    // name for police and citizen
-  name: { type: String},
+const UserSchema = new mongoose.Schema(
+  {
+    name: String,
 
-  // phone for citizen
-  phone: { type: String, required: function() { return this.role === 'citizen'; } },
+    phone: {
+      type: String,
+      required() {
+        return this.role === 'citizen';
+      }
+    },
 
-  // badge number only for police
-  badgeNumber: { type: String, required: function() { return this.role === 'police'; }},
+    badgeNumber: {
+      type: String,
+      required() {
+        return this.role === 'police';
+      }
+    },
 
-  password: { type: String },  
-  role: { 
-    type: String, 
-    enum: ['police', 'citizen', 'anonymous'], 
-    required: true 
+    password: String,
+
+    role: {
+      type: String,
+      enum: ['police', 'citizen', 'anonymous'],
+      required: true
+    }
+  },
+  {
+    timestamps: true
   }
-}, { timestamps: true });
+);
 
-const User = mongoose.model('User', UserSchema);
-export default User;
+export default mongoose.model('User', UserSchema);
